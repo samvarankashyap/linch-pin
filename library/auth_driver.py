3ee7702c340e79297f89c8948650f6281654a365
Samvaran Rallabandi <srallaba@redhat.com>
2016-11-02 16:50:43 -0400

    updated search function

--- library/auth_driver.py
+++ library/auth_driver.py
@@ -20,7 +20,6 @@ options:
 
 author: Samvaran Kashyap Rallabandi -
 '''
-
 from ansible.module_utils.basic import *
 import datetime
 import sys
@@ -29,7 +28,7 @@ import os
 import shlex
 import tempfile
 import yaml
-
+from credentials import get_creds, get_default_creds
 
 def check_file_paths(module, *args):
     for file_path in args:
@@ -40,17 +39,40 @@ def check_file_paths(module, *args):
         if os.path.isdir(file_path):
             module.fail_json(msg= "Recursive directory not supported  %s " % (file_path))
 
+def search_cred(module, cred_type, cred_store, cred_name):
+    try:
+        cred = get_creds(cred_type, cred_store, cred_name)
+        #cred = None
+        if cred == None:
+            cred = get_default_creds(cred_type, cred_store, cred_name) 
+        return cred
+    except Exception as e:
+        #module.fail_json(msg="Auth driver failed to fetch cred_obj:ERROR:"+str(e)) 
+        print e 
+
+"""
 def main():
     module = AnsibleModule(
     argument_spec={
-            'type':     {'required': True, 'aliases': ['auth_type']},
-            'creds_store': {'required': False, 'aliases': ['credential_store']},
+            'type': {'required': True, 'aliases': ['auth_type']},
+            'cred_store': {'required': True, 'aliases': ['credential_store']},
+            'cred_name': {'required': True, 'aliases': ['credential_name']},
         },
         required_one_of=[],
         supports_check_mode=True
     )
+    cred_type = module.params['type']
+    cred_store = module.params['cred_store']
+    cred_store = module.params['cred_name']
+    output = search_cred(cred_type, cred_store)
     changed = True
-    module.exit_json(changed=changed, output={})
+    module.exit_json(changed=changed, output=output)
 
 from ansible.module_utils.basic import *
 main()
+"""
+print search_cred("", "aws", "/root/cred_store", "testname")
+print search_cred("", "openstack", "/root/cred_store", "testname")
+print search_cred("", "gcloud", "/root/cred_store", "testname")
+print search_cred("", "example", "/root/cred_store", "testname")
+
