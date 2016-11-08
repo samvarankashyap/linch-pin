c46ce630cb436e2aad34cb248acf6898fb072550
Samvaran Rallabandi <srallaba@redhat.com>
2016-11-08 12:12:57 -0500

    updated module authdriver with credential package functions

--- library/auth_driver.py
+++ library/auth_driver.py
@@ -50,30 +50,39 @@ def search_cred(module, cred_type, cred_store, cred_name):
         #module.fail_json(msg="Auth driver failed to fetch cred_obj:ERROR:"+str(e)) 
         print e 
 
-
 def main():
     module = AnsibleModule(
     argument_spec={
             'type': {'required': True, 'aliases': ['auth_type']},
             'cred_store': {'required': True, 'aliases': ['credential_store']},
             'cred_name': {'required': True, 'aliases': ['credential_name']},
+            'profile': {'required': False, 'aliases': ['profile_name']},
         },
         required_one_of=[],
         supports_check_mode=True
     )
     cred_type = module.params['type']
     cred_store = module.params['cred_store']
-    cred_store = module.params['cred_name']
-    output = search_cred(cred_type, cred_store)
+    cred_name = module.params['cred_name']
+    profile = module.params['profile']
+    output = get_cred(cred_type, cred_store, profile)
     changed = True
     module.exit_json(changed=changed, output=output)
 
 from ansible.module_utils.basic import *
 main()
 
+"""
 #debug statements
+import pdb
+pdb.set_trace()
+print get_creds("aws","/root/linch-pin.work/credential_store","botoconfig")
+print get_creds("aws","/root/linch-pin.work/credential_store","botoconfig","another_profile")
+print get_creds("openstack","/root/linch-pin.work/credential_store","clouds","devstack")
+print get_creds("openstack","/root/linch-pin.work/credential_store","clouds","")
+print get_creds("gcloud","/root/linch-pin.work/credential_store","gcloud")
 #print search_cred("", "aws", "/root/linch-pin.work/credential_store", "testname")
 #print search_cred("", "openstack", "/root/linch-pin.work/credential_store", "testname")
 #print search_cred("", "gcloud", "/root/linch-pin.work/credential_store", "testname")
 #print search_cred("", "example", "/root/linch-pin.work/credential_store", "testname")
-
+"""
